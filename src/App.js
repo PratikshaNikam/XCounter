ea6c4bf4d7fc2bba6c12e0e6737cff3ab5fb3c46
Pratiksha Nikam <pratiksha.gawande@gmail.com>
2024-12-12 10:09:22 +0530

    created counter app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import React,{useState} from 'react';
+import './App.css';
+
+function App() {
+
+  const [count, setCount] = useState(0);
+  return (
+    <div className="App">
+      <header>
+        
+    <h1>Counter App</h1>
+      </header>
+
+      <p>Count: {count}</p>
+
+      <button onClick={() => setCount(count + 1)} className='button'>Increment</button>
+      <button onClick={() => setCount(count - 1)} className='button'>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
